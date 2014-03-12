bae9ff24bd2bdd282c544263449bc60ca3a7bb55
HIROSE Masaaki <hirose31@gmail.com>
2014-03-12 15:57:42 +0900

    Support RedHat, CentOS

---
+++ lib/Omnis/Agent/Handler/Metric/RedHat.pm
@@ -0,0 +1,25 @@
+package Omnis::Agent::Handler::Metric::RedHat;
+
+use strict;
+use warnings;
+use 5.010;
+use Carp;
+use utf8;
+
+use parent qw(Omnis::Agent::Handler::Metric::Linux);
+
+1;
+
+__END__
+
+# for Emacsen
+# Local Variables:
+# mode: cperl
+# cperl-indent-level: 4
+# cperl-close-paren-offset: -4
+# cperl-indent-parens-as-block: t
+# indent-tabs-mode: nil
+# coding: utf-8
+# End:
+
+# vi: set ts=4 sw=4 sts=0 et ft=perl fenc=utf-8 :
